b0f87c176c423626936dd4cf0e6d03f3ead93849
bryantbello <bryant.bello@outlook.com>
2016-04-13 21:14:01 -0400

    Completed student.js

---
+++ student.js
@@ -0,0 +1,20 @@
+var Student = function (name, grade, GPA, detentions, sleepingInClass, catchPhrase, canStudentHaveFun) {
+    this.name = name;
+    this.grade = grade;
+    this.GPA = GPA;
+    this.detentions = detentions;
+    this.sleepingInClass = sleepingInClass;
+    this.catchPhrase = catchPhrase;
+    this.canStudentHaveFun = function () {
+        if (this.GPA > 2 && this.detentions < 10) {
+            console.log("Congratulations, you can have fun!");
+            return true;
+        }
+        else {
+            console.log('You are in danger of failing. Please try harder!');
+            return false;
+        }
+    };
+};
+
+module.exports = Student;
